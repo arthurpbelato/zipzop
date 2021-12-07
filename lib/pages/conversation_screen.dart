64fa603d9cdcd9b10e847b40a98f73b18db98501
Christianrslv <christianrslv22@gmail.com>
2021-12-07 07:06:51 -0300

    Fix: Scroll Chat

--- lib/pages/conversation_screen.dart
+++ lib/pages/conversation_screen.dart
@@ -28,14 +28,18 @@ class _ConversationScreenState extends State<ConversationScreen> {
     return StreamBuilder<QuerySnapshot>(
       stream: chatMessagesStream,
       builder: (context, snapshots) {
-       if(snapshots.hasData)
+        if (snapshots.hasData)
           return ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
               itemCount: snapshots.data?.docs.length,
               itemBuilder: (context, index) {
-                return MessageTile(snapshots.data!.docs[index].get('message'),
-                    snapshots.data!.docs[index].get('sendBy') == Constants.myName);
+                return MessageTile(
+                    snapshots.data!.docs[index].get('message'),
+                    snapshots.data!.docs[index].get('sendBy') ==
+                        Constants.myName);
               });
-         return Center(child: CircularProgressIndicator());
+        return Center(child: CircularProgressIndicator());
       },
     );
   }
@@ -63,7 +67,9 @@ class _ConversationScreenState extends State<ConversationScreen> {
     setState(() {
       isLoading = true;
     });
-    await databaseMethods.getConversationMessages(widget.chatRoomId).then((value) {
+    await databaseMethods
+        .getConversationMessages(widget.chatRoomId)
+        .then((value) {
       setState(() {
         chatMessagesStream = value;
         isLoading = false;
@@ -76,59 +82,61 @@ class _ConversationScreenState extends State<ConversationScreen> {
     return Scaffold(
       appBar: appBarMain(context),
       body: Container(
-              child: Stack(
-                children: [
-                  ChatMessageList(),
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(20.0),
-                          bottomRight: Radius.circular(20.0),
+        child: Stack(
+          children: [
+            Container(
+                padding: EdgeInsets.only(bottom: 59),
+                child: ChatMessageList()),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 24),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: messageController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite uma mensagem...',
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none,
                         ),
                       ),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5, horizontal: 24),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              controller: messageController,
-                              decoration: InputDecoration(
-                                hintText: 'Digite uma mensagem...',
-                                hintStyle: TextStyle(color: Colors.black54),
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () => sendMessage(),
-                            child: Container(
-                              height: 40,
-                              width: 40,
-                              decoration: BoxDecoration(
-                                color: Colors.teal,
-                                borderRadius: BorderRadius.circular(40.0),
-                              ),
-                              padding: EdgeInsets.zero,
-                              child: Icon(
-                                Icons.send,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
+                    ),
+                    GestureDetector(
+                      onTap: () => sendMessage(),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.teal,
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
+                        padding: EdgeInsets.zero,
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
+          ],
+        ),
+      ),
     );
   }
 }
+
 // isLoading? Center(child: CircularProgressIndicator()) :
 class MessageTile extends StatelessWidget {
   final String message;
@@ -145,31 +153,21 @@ class MessageTile extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 4),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: isSendByMe ? [
-              const Color(0xffdca65a),
-              const Color(0xffe87bca)
-            ] : [
-              const Color(0xff6b97bd),
-              const Color(0xffadecb4)
-            ]
-          ),
-          borderRadius: isSendByMe ?
-              BorderRadius.only(
-                topLeft: Radius.circular(23),
-                topRight: Radius.circular(23),
-                bottomLeft: Radius.circular(23)
-              ) :
-              BorderRadius.only(
-                topLeft: Radius.circular(23),
-                topRight: Radius.circular(23),
-                bottomRight: Radius.circular(23)
-              )
-        ),
-        child: Text(message, style: TextStyle(
-          color: Colors.white,
-          fontSize: 18
-        )),
+            gradient: LinearGradient(
+                colors: isSendByMe
+                    ? [const Color(0xffdca65a), const Color(0xffe87bca)]
+                    : [const Color(0xff6b97bd), const Color(0xffadecb4)]),
+            borderRadius: isSendByMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomLeft: Radius.circular(23))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomRight: Radius.circular(23))),
+        child:
+            Text(message, style: TextStyle(color: Colors.white, fontSize: 18)),
       ),
     );
   }
